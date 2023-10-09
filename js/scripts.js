b056714bc13c293578c70a9e9e8e9e5b70576916
ANAS <anaspappadan@gmail.com>
2023-10-09 20:08:04 +0530

    random number variable added

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
-var contents = document.getElementById("content_wrapper")
- contents.style.display = "none";
-  
- 
-function unwrap() {
-    var el = document.getElementById("content_wrapper");
-    var play = document.getElementById("play");
-if (el.style.display == "none") el.style.display = "flex"; play.style.display="none"; 
+var moleImage = document.getElementsByClassName("moleImg");
+var contents = document.getElementById("content_wrapper");
+var playButton = document.getElementById("play")
+
+var randomNum = Math.round(Math.random() * 9);
+function playClick() {
+  var play = document.getElementById("play");
+  play.innerHTML = "Restart";
+  moleImage[randomNum].style.display = "block";
 }
 
+function whack() {
+  moleImage[randomNum].style.display = "none";
+  randomNum = Math.round(Math.random() * 9);
+  moleImage[randomNum].style.display = "block";
+  console.log(moleImage[randomNum])
+}
+
+playButton.addEventListener("click",function(){playClick()})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function unwrap() {
+//   var el = document.getElementById("content_wrapper");
+//   var play = document.getElementById("play");
+//   if (el.style.display == "none") el.style.display = "flex";
+//   play.style.display = "none";
+// }
